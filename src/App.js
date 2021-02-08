155aaa94c8735e15fb02f140635c10f5d9da5584
andrewbastian <andrew.bastian@gmail.com>
2021-02-08 12:57:54 -0800

    New page header

--- src/App.js
+++ src/App.js
@@ -1,71 +1,73 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
-import {Provider, defaultTheme, Flex, Grid, View, Image} from '@adobe/react-spectrum';
+import { Provider, defaultTheme, Flex, Grid, View, Image } from '@adobe/react-spectrum';
 import About from './components/About';
+import Resume from './components/Resume';
 import Work from './components/Work';
 import Footer from './components/Footer';
 import Contact from './components/Contact';
 import desktopImage from './assets/Desk-bg.jpeg';
 // import mobileImage from './Mob-bg.jpeg';
 import LoadingScreen from './components/LoadingScreen';
-import NamePlate from './assets/websiteHeaderWhite.svg';
+import NamePlate from './assets/HeaderGradient.gif';
 
 function App() {
-    const [loading, setLoading] = useState(true)
-       useEffect(() => {
-           setTimeout(() => setLoading(false), 6000)
-       }, []);
-  return (
-    <>
-    { loading === false ? (
-    <Provider theme={defaultTheme} colorScheme='dark'>
+    const [loading, setLoading] = useState(true);
+    useEffect(() => {
+        setTimeout(() => setLoading(false), 6000);
+    }, []);
+    return (
+        <>
+            {loading === false ? (
+                <Provider theme={defaultTheme} colorScheme='dark'>
+                    <div className='App' style={{ backgroundImage: `url(${desktopImage})` }}>
+                        <Grid
+                            areas={['header  header', 'content content', 'footer  footer']}
+                            columns={['1fr', '3fr']}
+                            rows={['auto', 'auto', 'auto']}
+                            height='auto'
+                            gap='size-100'
+                        >
+                            <View
+                                gridArea='header'
+                                display='flex'
+                                justifySelf='center'
+                                alignSelf='center'
+                                marginTop='5%'
+                                width='50%'
+                            >
+                                <Image src={NamePlate} alt='NamePlateWhite' margin='auto' justifySelf='center' />
+                            </View>
 
-      <div className="App" style={{backgroundImage: `url(${desktopImage})` }}>
-          <Grid
-            areas={['header  header', 'content content', 'footer  footer']}
-            columns={['1fr', '3fr']}
-            rows={['auto', 'auto', 'auto']}
-            height="auto"
-            gap="size-100"
-          >
-
-            <View gridArea="header" display="flex" justifySelf="center" alignSelf="center" marginTop='5%' width='size-5000'>
-            
-            <Image  src={NamePlate} alt="NamePlateWhite" margin="auto" justifySelf='center'
-            
-        />
-             
-             </View>
-
-            <View 
-            gridArea="content" 
-            display="flex" 
-            justifySelf="center" 
-            marginTop="size-100" > 
-
-            <Flex direction="column" width="size-2000" gap="size-200" marginTop="20%">
-              <About/>
-              <Work/>
-              <Contact/>
-             </Flex>
-
-            </View>
-            <View gridArea="footer" margin="auto" 
-            >
-            <Flex direction="row" width="size-2000" gap="size-1000" alignSelf="center" justifySelf = "center" marginTop="25%" paddingLeft="20px"
-            
-            >
-            <Footer/>
-            </Flex>
-            </View>
-          </Grid>
-        </div>
-    </Provider>
-    ) : (
-    <LoadingScreen />
-    )}
-   </>
-  );
+                            <View gridArea='content' display='flex' justifySelf='center' marginTop='size-100'>
+                                <Flex direction='column' width='size-2000' gap='size-200' marginTop='20%'>
+                                    <About />
+                                    <Work />
+                                    <Contact />
+                                    <Resume />
+                                </Flex>
+                            </View>
+                            <View gridArea='footer' margin='auto'>
+                                <Flex
+                                    direction='row'
+                                    width='size-2000'
+                                    gap='size-1000'
+                                    alignSelf='center'
+                                    justifySelf='center'
+                                    marginTop='25%'
+                                    paddingLeft='20px'
+                                >
+                                    <Footer />
+                                </Flex>
+                            </View>
+                        </Grid>
+                    </div>
+                </Provider>
+            ) : (
+                <LoadingScreen />
+            )}
+        </>
+    );
 }
 
 export default App;
