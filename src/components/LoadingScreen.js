ea9f01ed8336284d0f979634dc4732b87369550e
andrewbastian <andrew.bastian@gmail.com>
2021-02-08 12:58:46 -0800

    New Loading gif

--- src/components/LoadingScreen.js
+++ src/components/LoadingScreen.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import Loading from '../assets/Loading.gif';
+import Loading from '../assets/Com64Loading.gif';
 const LoadingScreen = () => {
     return (
         <div>
-        	<img src={Loading} alt="loadingpage" />
+            <img src={Loading} alt='loadingpage' style={{ width: '100%' }} />
         </div>
     );
 };
-
 
 export default LoadingScreen;
