ae6921e39d702124fb8c811a108a45956555394c
andrewbastian <andrew.bastian@hey.com>
2022-09-14 21:56:15 -0700

    Everything else

--- src/Menu.js
+++ src/Menu.js
@@ -1,39 +1,40 @@
-import React from 'react';
-import { Text, MenuTrigger, Menu, ActionButton, Item } from '@adobe/react-spectrum';
-import ShowMenu from '@spectrum-icons/workflow/ShowMenu';
+/*import React from "react";
+import {
+  Text,
+  MenuTrigger,
+  Menu,
+  ActionButton,
+  Item,
+} from "@adobe/react-spectrum";
+import ShowMenu from "@spectrum-icons/workflow/ShowMenu";
+
 const NavMenu = () => {
-    return (
-  <MenuTrigger>
-  <ActionButton width='25%' display="flex" alignSelf="center" marginStart='3%'
-  ><ShowMenu/></ActionButton>
-  <Menu 
-  
-  >
-
-    <Item key="Home" textValue="Home">
-      
-      <Text>Home</Text>
-
-    </Item>
-    <Item key="About" textValue="About">
-      
-      <Text>About</Text>
-
-    </Item>
-    <Item key="Work" textValue="Work">
-      
-      <Text>Work</Text>
-
-    </Item>
-    <Item key="Contact" textValue="Contact">
-      
-      <Text>Contact</Text>
-
-    </Item>
-  </Menu>
-</MenuTrigger>
-    );
+  return (
+    <MenuTrigger>
+      <ActionButton
+        width="25%"
+        display="flex"
+        alignSelf="center"
+        marginStart="3%"
+      >
+        <ShowMenu />
+      </ActionButton>
+      <Menu onAction={(key)= alert(key)}>
+        <Item key="Home" textValue="Home">
+          <Text>Home</Text>
+        </Item>
+        <Item key="About" textValue="About">
+          <Text>About</Text>
+        </Item>
+        <Item key="Work" textValue="Work">
+          <Text>Work</Text>
+        </Item>
+        <Item key="Contact" textValue="Contact">
+          <Text>Contact</Text>
+        </Item>
+      </Menu>
+    </MenuTrigger>
+  );
 };
 
-
-export default NavMenu;
+export default NavMenu;*/
